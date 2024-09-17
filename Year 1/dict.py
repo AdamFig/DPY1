5a58b18e447e9842576ddce43ffb0789996c831d
Adam Figlewicz <26figlewicz_a@aswarsaw.org>
2024-09-17 11:41:41 +0200

    Added a new file

---
+++ Year 1/dict.py
@@ -0,0 +1,7 @@
+import os
+os.system('clear')
+
+# Problem 1: Create a Colony Dictionary
+
+thalaxion_colony = {'name': "New Horizon",'population': "5000", 'resources': ["water", "minerals", "oxygen"], 'established': "2120"}
+print(thalaxion_colony)
